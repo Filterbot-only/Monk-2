0d21e8d95d5a244e3cf12606b2aab65894282181
Goopalamoorthy <142672172+Goopalamoorthy@users.noreply.github.com>
2023-08-24 11:15:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://teamhdboss:samarkhann@manimaaran.ywya2zt.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "manimaaran")
+DATABASE_NAME = environ.get('DATABASE_NAME', "teamhdboss")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
