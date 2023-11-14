31a44ece20a2f62cbbfd7a585820a5a21d0165c7
Filterbot-only <147235633+Filterbot-only@users.noreply.github.com>
2023-11-14 10:25:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,8 +71,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', "sxslink.com")
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', "ede92b02b7851b43700b931c696ab2de970f39f4")
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', "krownlinks.com")
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', "157d47556b608adf482c93f3a950452e46611ce0")
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
